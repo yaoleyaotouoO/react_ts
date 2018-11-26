f136b8fe4a2593965e70db7845d6beefb8c4ec99
xjx <644233452@qq.com>
2018-11-26 14:35:50 +0800

    add dropdown code

--- src/components/dropDownWithFilter/index.tsx
+++ src/components/dropDownWithFilter/index.tsx
@@ -17,6 +17,7 @@ export interface IDropDownConfing {
 export interface IDropDownWithFilterProps {
     height?: number;
     data: IDropDownConfing;
+    placeholder: string;
     toggleCheck: Function;
     toggleCheckAll: Function;
 }
@@ -51,17 +52,17 @@ class ContentLi extends React.Component<IContentLiProps, IContentLiState> {
             selectedIds.indexOf(item.id) > -1 ? sortData.unshift(item) : sortData.push(item);
         });
 
-        return sortData.map(data => {
-            return <li key={data.id}>
+        return sortData.filter(item => item.name.match(new RegExp(filterText, 'ig'))).slice(0, 300).map(item => {
+            return <li key={item.id}>
                 <div className={style.form_check}>
                     <label className={style.form_check_label}>
                         <input
                             type="checkbox"
                             className={style.form_check_input}
-                            value={data.id}
-                            checked={selectedIds.indexOf(data.id) > -1}
-                            onChange={() => toggleCheck(data)} />
-                        <span>{data.name}</span>
+                            value={item.id}
+                            checked={selectedIds.indexOf(item.id) > -1}
+                            onChange={(e) => toggleCheck(e.target.checked, item.id)} />
+                        <span>{item.name}</span>
                     </label>
                 </div>
             </li>;
@@ -74,7 +75,7 @@ const DEFAULT_HEIGHT = 350;
 export class DropDownWithFilter extends React.Component<IDropDownWithFilterProps, IDropDownWithFilterState>{
     input: HTMLInputElement;
     scroll: Scrollbars;
-    groupNode: React.Ref<HTMLDivElement>;
+    dropDownNode: React.Ref<HTMLDivElement>;
     ulNode: React.Ref<HTMLDivElement>;
 
     constructor(props: IDropDownWithFilterProps) {
@@ -87,33 +88,91 @@ export class DropDownWithFilter extends React.Component<IDropDownWithFilterProps
             height
         }
 
-        this.groupNode = React.createRef()
+        this.dropDownNode = React.createRef()
         this.ulNode = React.createRef()
     }
 
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    componentWillReceiveProps(nextProps: IDropDownWithFilterProps) {
+        let count = nextProps.data.rawData.length;
+        this.autoScrollHandle(count ? count++ : count);
+    }
+
     getPlaceHolderText = () => {
-        return 'Filter by Service Category';
+        const { data: { selectedIds, rawData }, placeholder } = this.props;
+
+        if (!selectedIds.length) {
+            return placeholder;
+        } else if (selectedIds.length === rawData.length) {
+            return `All selected(${rawData.length})`;
+        } else if (selectedIds.length >= 4) {
+            return `selected(${selectedIds.length})`;
+        }
+
+        let textArr: string[] = [];
+        rawData.map(x => {
+            if (selectedIds.indexOf(x.id) > -1) {
+                textArr.push(x.name);
+            }
+        });
+
+        return textArr.join(', ');
     }
 
     toggleSelection = () => {
         this.setState({ open: !this.state.open });
+
+        if (this.input) {
+            setTimeout(() => {
+                this.input.focus();
+            }, 100);
+        }
     }
 
-    searchTextOnchange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    searchTextOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const items = this.props.data.rawData;
+        let filterText = e.target.value;
+        let count = items.filter(item => item.name.match(new RegExp(filterText, 'ig'))).length;
 
+        if (!filterText) {
+            count = this.props.data.rawData.length;
+            count ? count++ : count;
+        }
+
+        this.autoScrollHandle(count);
+        this.setState({ filterText });
     }
 
     clearSearchText = () => {
-
+        let count = this.props.data.rawData.length;
+        this.autoScrollHandle(count ? count++ : count);
+        this.setState({ filterText: '' });
     }
 
-    onToggleCheckAll = () => {
-        this.props.toggleCheckAll();
+    onToggleCheck = (checked: boolean, checkedId: number) => {
+        let top = this.scroll.getScrollTop();
+        requestAnimationFrame(() => {
+            this.scroll.scrollTop(top);
+        })
+
+        this.props.toggleCheck(checked, checkedId);
+    }
+
+    autoScrollHandle = (count: number) => {
+        const { height = DEFAULT_HEIGHT } = this.props;
+        this.setState({ height: count * 37 >= height ? height : count * 37 });
     }
 
     render() {
         const { open, filterText, height } = this.state;
-        const { data: { rawData, selectedIds }, toggleCheck } = this.props;
+        const { data: { rawData, selectedIds }, toggleCheckAll } = this.props;
 
         const showSelectAll = this.state.filterText === '' && rawData.length !== 0;
         const selectedAll = rawData.length === selectedIds.length;
@@ -124,73 +183,70 @@ export class DropDownWithFilter extends React.Component<IDropDownWithFilterProps
             return open ? `${defaultStyle} ${style.show}` : defaultStyle;
         }
 
-        return <div className={style.select_body}>
-            <div
-                className={`${style.search_group} ${style.dropdown_filter}`}
-                ref={this.groupNode}>
-                <input
-                    readOnly
-                    type="text"
-                    className={style.form_control}
-                    placeholder={this.getPlaceHolderText()}
-                    onClick={() => this.toggleSelection()} />
-                <ul className={getDropdownStyle()}>
-                    <li className={style.filter} value="0">
-                        <div className={style.input_group}>
-                            <span className={style.input_group_addon}>
-                                <i className={`${style.fa} ${style.fa_search}`}></i>
-                            </span>
-                            <input
-                                type="text"
-                                value={filterText}
-                                onChange={(e) => this.searchTextOnchange(e)}
-                                className={style.form_control}
-                                placeholder="Search"
-                                ref={(dom) => { this.input = dom; }} />
-                            <span
-                                onClick={() => this.clearSearchText()}
-                                className={style.input_group_addon}>
-                                <a className={style.multi_clear_filter}>
-                                    <i className={style.mdi_navigation_close}></i>
-                                </a>
-                            </span>
-                        </div>
-                    </li>
+        return <div className={`${style.search_group} ${style.dropdown_filter}`} ref={this.dropDownNode}>
+            <input
+                readOnly
+                type="text"
+                className={style.form_control}
+                placeholder={this.getPlaceHolderText()}
+                onClick={() => this.toggleSelection()} />
 
-                    <Scrollbars
-                        ref={(dom) => { this.scroll = dom; }}
-                        style={{ height: height }}
-                        autoHide>
-                        <div ref={this.ulNode}>
-                            {
-                                showSelectAll ?
-                                    <li>
-                                        <div className={style.form_check}>
-                                            <label className={style.form_check_label}>
-                                                <input
-                                                    type="checkbox"
-                                                    checked={selectedAll}
-                                                    onChange={() => this.onToggleCheckAll()}
-                                                    className={style.form_check_input} />
-                                                <span>Select All</span>
-                                            </label>
-                                        </div>
-                                    </li> :
-                                    null
-                            }
-                            {
-                                open ?
-                                    <ContentLi
-                                        data={rawData}
-                                        selectedIds={selectedIds}
-                                        toggleCheck={() => { }}
-                                        filterText={filterText} /> :
-                                    null
-                            }
-                        </div>
-                    </Scrollbars>
-                </ul>
-            </div>
-        </div>
+            <ul className={getDropdownStyle()}>
+                <li className={style.filter}>
+                    <div className={style.input_group}>
+                        <span className={style.input_group_addon}>
+                            <i className={`${style.fa} ${style.fa_search}`}></i>
+                        </span>
+                        <input
+                            type="text"
+                            value={filterText}
+                            onChange={(e) => this.searchTextOnChange(e)}
+                            className={style.form_control}
+                            placeholder="Search"
+                            ref={(dom) => { this.input = dom; }} />
+                        <span
+                            onClick={() => this.clearSearchText()}
+                            className={style.input_group_addon}>
+                            <a className={style.multi_clear_filter}>
+                                <i className={style.mdi_navigation_close}></i>
+                            </a>
+                        </span>
+                    </div>
+                </li>
+
+                <Scrollbars
+                    ref={(dom) => { this.scroll = dom; }}
+                    style={{ height }}
+                    autoHide>
+                    <div ref={this.ulNode}>
+                        {
+                            showSelectAll ?
+                                <li>
+                                    <div className={style.form_check}>
+                                        <label className={style.form_check_label}>
+                                            <input
+                                                type="checkbox"
+                                                checked={selectedAll}
+                                                onChange={(e) => toggleCheckAll(e.target.checked)}
+                                                className={style.form_check_input} />
+                                            <span>Select All</span>
+                                        </label>
+                                    </div>
+                                </li> :
+                                null
+                        }
+                        {
+                            open ?
+                                <ContentLi
+                                    data={rawData}
+                                    selectedIds={selectedIds}
+                                    toggleCheck={(checked: boolean, checkedId: number) => this.onToggleCheck(checked, checkedId)}
+                                    filterText={filterText} /> :
+                                null
+                        }
+                    </div>
+                </Scrollbars>
+            </ul>
+        </div>;
     }
 }
--- src/containers/design/components/select/index.tsx
+++ src/containers/design/components/select/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { DropDownWithFilter, IIdName, IDropDownConfing } from '../../../../components/dropDownWithFilter';
+import { DropDownWithFilter, IIdName } from '../../../../components/dropDownWithFilter';
 
 
 export interface ISelectProps {
@@ -8,39 +8,68 @@ export interface ISelectProps {
 }
 
 export interface ISelectState {
-
+    selectedIds: number[];
+    rawData: IIdName[];
 }
 
 export default class Select extends React.Component<ISelectProps, ISelectState>{
     constructor(props: ISelectProps) {
         super(props);
+
+        this.state = {
+            selectedIds: [],
+            rawData: [
+                { id: 1, name: 'Animal Habitat 1' },
+                { id: 2, name: 'Animal Habitat 2' },
+                { id: 3, name: 'Animal Habitat 3' },
+                { id: 4, name: 'Animal Habitat 4' },
+                { id: 5, name: 'Animal Habitat 5' },
+                { id: 6, name: 'Animal Habitat 6' },
+                { id: 7, name: 'Animal Habitat 7' },
+                { id: 8, name: 'Animal Habitat 8' },
+                { id: 9, name: 'Animal Habitat 9' },
+                { id: 10, name: 'Animal Habitat 10' },
+                { id: 11, name: 'Animal Habitat 11' },
+                { id: 12, name: 'Animal Habitat 12' },
+            ]
+        }
     }
 
-    toggleCheck = () => {
-        return true;
+    toggleCheck = (checked: boolean, checkedId: number) => {
+        let { selectedIds } = this.state;
+
+        if (!checked) {
+            selectedIds = selectedIds.filter(x => x !== checkedId);
+            this.setState({ selectedIds });
+            return;
+        }
+
+        selectedIds.push(checkedId);
+        this.setState({ selectedIds });
     }
 
-    toggleCheckAll = () => {
+    toggleCheckAll = (checked: boolean) => {
+        const { rawData } = this.state;
 
+        if (!checked) {
+            this.setState({ selectedIds: [] });
+            return;
+        }
+
+        let selectedIds = rawData.map(x => x.id);
+        this.setState({ selectedIds });
     }
 
     render() {
-        const rawData: IIdName[] = [
-            { id: 1, name: 'test1' },
-            { id: 2, name: 'test2' },
-            { id: 3, name: 'test3' }
-        ]
+        const { rawData, selectedIds } = this.state;
 
-        const data: IDropDownConfing = {
-            rawData,
-            selectedIds: [1, 2]
-        }
-
-        return <DropDownWithFilter
-            data={data}
-            toggleCheck={() => this.toggleCheck()}
-            toggleCheckAll={() => this.toggleCheckAll()}
-        />;
+        return <div style={{ width: '216px', padding: '5px' }}>
+            <DropDownWithFilter
+                data={{ rawData, selectedIds }}
+                placeholder="Filter by Service Category"
+                toggleCheck={(checked: boolean, checkedId: number) => this.toggleCheck(checked, checkedId)}
+                toggleCheckAll={(checked: boolean) => this.toggleCheckAll(checked)}
+            />
+        </div>;
     }
-
 }
